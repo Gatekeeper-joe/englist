4b58d9b4ae86819b2cc190ecf7b0af964d011298
Gatekeeper-joe <investorjk5x@gmail.com>
2023-02-15 08:22:02 +0900

    initial commit

---
+++ src/test/java/englistapi/EnglistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package englistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
